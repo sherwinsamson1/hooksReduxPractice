2286afc2b2238d797775c1a208008d2ae1121a2a
sherwinsamson1 <sherwinsamson1@gmail.com>
2019-12-13 15:14:26 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+//REDUX
+import { createStore, combineReducers } from "redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
+
+function counterReducer(state = { count: 0 }, action) {
+	switch (action.type) {
+		case "INCREMENT_COUNT":
+			return {
+				...state,
+				count: state.count + 1
+			};
+		case "DECREMENT_COUNT":
+			return {
+				...state,
+				count: state.count - 1
+			};
+
+		default:
+			return state;
+	}
+}
+
+function nameReducer(state = { name: "" }, action) {
+	switch (action.type) {
+		case "UPDATE_NAME":
+			return {
+				...state,
+				name: action.payload
+			};
+		default:
+			return state;
+	}
+}
+
+const rootReducer = combineReducers({
+	counterReducer,
+	nameReducer
+});
+const INITIAL_STATE = {};
+const store = createStore(rootReducer, INITIAL_STATE);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Provider store={store}>
+			<Counter />
+			<Name />
+		</Provider>
+	);
+}
+
+function Counter() {
+	const { count, name } = useSelector(state => ({
+		...state.counterReducer,
+		...state.nameReducer
+	}));
+	const dispatch = useDispatch();
+
+	function incrementCount() {
+		dispatch({
+			type: "INCREMENT_COUNT"
+		});
+	}
+
+	function decrementCount() {
+		dispatch({
+			type: "DECREMENT_COUNT"
+		});
+	}
+
+	return (
+		<div>
+			<h2>Counter: {count}</h2>
+			<button onClick={incrementCount}>+</button>
+			<button onClick={decrementCount}>-</button>
+			<div>
+				<h3>Your name is: {name}</h3>
+			</div>
+		</div>
+	);
+}
+
+function Name() {
+	const dispatch = useDispatch();
+
+	function handleUpdateName(event) {
+		dispatch({
+			type: "UPDATE_NAME",
+			payload: event.target.value
+		});
+	}
+
+	return (
+		<div>
+			<input placeholder="Input name here" onChange={handleUpdateName} />
+		</div>
+	);
 }
 
 export default App;
